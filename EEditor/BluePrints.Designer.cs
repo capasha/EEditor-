e6b09b87549a9900650050bcc83f5481374cb13d
capasha <capashaa@gmail.com>
2020-08-15 20:48:30 +0200

    Added blueprint. But does not work because it's in beta.

---
+++ EEditor/BluePrints.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace EEditor
+{
+    partial class BluePrints
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SaveBPButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 343);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SaveBPButton
+            // 
+            this.SaveBPButton.Location = new System.Drawing.Point(12, 361);
+            this.SaveBPButton.Name = "SaveBPButton";
+            this.SaveBPButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveBPButton.TabIndex = 1;
+            this.SaveBPButton.Text = "Save";
+            this.SaveBPButton.UseVisualStyleBackColor = true;
+            this.SaveBPButton.Click += new System.EventHandler(this.SaveBPButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(93, 361);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 2;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // BluePrints
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveBPButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "BluePrints";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BluePrints";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BluePrints_FormClosing);
+            this.Load += new System.EventHandler(this.BluePrints_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button SaveBPButton;
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
---
+++ EEditor/BluePrints.cs
@@ -0,0 +1,235 @@
+﻿using EELVL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EEditor
+{
+    public partial class BluePrints : Form
+    {
+        private Bitmap img4 = new Bitmap(25 * 16, 25 * 16);
+        private Bitmap img1 = new Bitmap(25 * 16, 25 * 16);
+        private string test = $"{Directory.GetCurrentDirectory()}\\test.json";
+        private BlockContainer container = new BlockContainer();
+
+        public BluePrints()
+        {
+            InitializeComponent();
+            var container = new BlockContainer() { Blocks = new List<Block>() };
+        }
+
+        private void BluePrints_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsData("EEBlueprints"))
+            {
+
+                string[][,] data = (string[][,])Clipboard.GetData("EEBlueprints");
+                if (data?.Length == 9)
+                {
+                    using (Graphics gr = Graphics.FromImage(img4))
+                    {
+                        gr.Clear(Color.Gray);
+                    }
+                    for (int y = 0; y < data[1].GetLength(0); y++)
+                    {
+                        for (int x = 0; x < data[1].GetLength(1); x++)
+                        {
+                            int bid1 = Convert.ToInt32(data[1][y, x]);
+
+
+                            if (bid1 >= 500 && bid1 <= 999)
+                            {
+                                if (MainForm.backgroundBMI[bid1] != 0 || bid1 == 500)
+                                {
+                                    container.Blocks.Add(new Block() { BlockID = bid1, Layer = 1, X = x, Y = y });
+                                    using (Graphics gr1 = Graphics.FromImage(img4))
+                                    {
+                                        gr1.DrawImage(MainForm.backgroundBMD.Clone(new Rectangle(MainForm.backgroundBMI[bid1] * 16, 0, 16, 16), MainForm.backgroundBMD.PixelFormat), x * 16, y * 16);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                    for (int y = 0; y < data[0].GetLength(0); y++)
+                    {
+                        for (int x = 0; x < data[0].GetLength(1); x++)
+                        {
+                            int bid = Convert.ToInt32(data[0][y, x]);
+
+                            using (Graphics gr = Graphics.FromImage(img4))
+                            {
+                                if (bid < 500 || bid >= 1001)
+                                {
+                                    if (MainForm.decosBMI[bid] != 0)
+                                    {
+                                        img1 = bdata.getRotation(bid, Convert.ToInt32(data[2][y, x]));
+                                        if (img1 != null)
+                                        {
+                                            gr.DrawImage(img1, new Rectangle(x * 16, y * 16, 16, 16));
+                                            container.Blocks.Add(new Block() { BlockID = bid, Layer = 0, X = x, Y = y, Param = Convert.ToInt32(data[2][y, x]) });
+                                        }
+                                        else
+                                        {
+                                            container.Blocks.Add(new Block() { BlockID = bid, Layer = 0, X = x, Y = y });
+                                            gr.DrawImage(MainForm.decosBMD.Clone(new Rectangle(MainForm.decosBMI[Convert.ToInt32(data[0][y, x])] * 16, 0, 16, 16), MainForm.decosBMD.PixelFormat), x * 16, y * 16);
+                                        }
+                                    }
+                                    else if (MainForm.miscBMI[bid] != 0 || bid == 119)
+                                    {
+                                        img1 = bdata.getRotation(bid, Convert.ToInt32(data[2][y, x]));
+                                        if (img1 != null)
+                                        {
+                                            gr.DrawImage(img1, new Rectangle(x * 16, y * 16, 16, 16));
+                                            container.Blocks.Add(new Block() { BlockID = bid, Layer = 0, X = x, Y = y, Param = Convert.ToInt32(data[2][y, x]) });
+                                        }
+                                        else
+                                        {
+                                            container.Blocks.Add(new Block() { BlockID = bid, Layer = 0, X = x, Y = y });
+                                            gr.DrawImage(MainForm.miscBMD.Clone(new Rectangle(MainForm.miscBMI[Convert.ToInt32(data[0][y, x])] * 16, 0, 16, 16), MainForm.miscBMD.PixelFormat), x * 16, y * 16);
+                                        }
+                                    }
+                                    else if (bid != 0)
+                                    {
+                                        container.Blocks.Add(new Block() { BlockID = bid, Layer = 0, X = x, Y = y });
+                                        gr.DrawImage(MainForm.foregroundBMD.Clone(new Rectangle(MainForm.foregroundBMI[Convert.ToInt32(data[0][y, x])] * 16, 0, 16, 16), MainForm.foregroundBMD.PixelFormat), x * 16, y * 16);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    using (Graphics gr = Graphics.FromImage(img4))
+                    {
+                        gr.DrawRectangle(new Pen(Color.Black), new Rectangle(0, 0, data[0].GetLength(1) * 16 - 1, data[0].GetLength(0) * 16 - 1));
+                    }
+                    //Clipboard.Clear();
+                    //Console.WriteLine(data[0]);
+                    pictureBox1.Image = img4;
+                    pictureBox1.Width = data[0].GetLength(1) * 16;
+                    pictureBox1.Height = data[0].GetLength(0) * 16;
+
+                }
+                //;
+                //Console.WriteLine(data[0]);
+                Clipboard.Clear();
+
+            }
+            // Clipboard.Clear();
+        }
+
+        private void BluePrints_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void SaveBPButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(test)) File.Delete(test);
+            File.WriteAllText(test, JsonConvert.SerializeObject(container, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            string json = null;
+
+            if (File.Exists(test))
+            {
+                var serializer = new JsonSerializer();
+
+                using (var sw = new StreamReader(test))
+                using (var reader = new JsonTextReader(sw))
+                {
+                    json = serializer.Deserialize(reader).ToString();
+                }
+            }
+            if (json != null)
+            {
+                container = JsonConvert.DeserializeObject<BlockContainer>(json);
+                foreach (var data in container.Blocks)
+                {
+                    if (data.Layer == 1)
+                    {
+                        if (data.BlockID >= 500 && data.BlockID <= 999)
+                        {
+                            if (MainForm.backgroundBMI[data.BlockID] != 0 || data.BlockID == 500)
+                            {
+                                using (Graphics gr1 = Graphics.FromImage(img4))
+                                {
+                                    gr1.DrawImage(MainForm.backgroundBMD.Clone(new Rectangle(MainForm.backgroundBMI[data.BlockID] * 16, 0, 16, 16), MainForm.backgroundBMD.PixelFormat), data.X * 16, data.Y * 16);
+                                }
+                            }
+                        }
+                    }
+                    if (data.Layer == 0)
+                    {
+                        using (Graphics gr = Graphics.FromImage(img4))
+                        {
+                            if (data.BlockID < 500 || data.BlockID >= 1001)
+                            {
+                                if (MainForm.decosBMI[data.BlockID] != 0)
+                                {
+                                    if (data.Param == null) img1 = null;
+                                    else img1 = bdata.getRotation(data.BlockID, Convert.ToInt32(data.Param));
+                                    if (img1 != null)
+                                    {
+                                        gr.DrawImage(img1, new Rectangle(data.X * 16, data.Y * 16, 16, 16));
+
+                                    }
+                                    else
+                                    {
+                                        gr.DrawImage(MainForm.decosBMD.Clone(new Rectangle(MainForm.decosBMI[data.BlockID] * 16, 0, 16, 16), MainForm.decosBMD.PixelFormat), data.X * 16, data.Y * 16);
+                                    }
+                                }
+                                else if (MainForm.miscBMI[data.BlockID] != 0 || data.BlockID == 119)
+                                {
+                                    if (data.Param == null) img1 = null;
+                                    else img1 = bdata.getRotation(data.BlockID, Convert.ToInt32(data.Param));
+                                    if (img1 != null)
+                                    {
+                                        gr.DrawImage(img1, new Rectangle(data.X * 16, data.Y * 16, 16, 16));
+
+                                    }
+                                    else
+                                    {
+
+                                        gr.DrawImage(MainForm.miscBMD.Clone(new Rectangle(MainForm.miscBMI[Convert.ToInt32(data.BlockID)] * 16, 0, 16, 16), MainForm.miscBMD.PixelFormat), data.X * 16, data.Y * 16);
+                                    }
+                                }
+                                else if (data.BlockID != 0)
+                                {
+
+                                    gr.DrawImage(MainForm.foregroundBMD.Clone(new Rectangle(MainForm.foregroundBMI[Convert.ToInt32(data.BlockID)] * 16, 0, 16, 16), MainForm.foregroundBMD.PixelFormat), data.X * 16, data.Y * 16);
+                                }
+                            }
+                        };
+                    }
+                }
+                pictureBox1.Image = img4;
+            }
+        }
+
+        private void ClipBoardButton_Click(object sender, EventArgs e)
+        {
+        }
+    }
+    public class Block
+    {
+        public int BlockID { get; set; }
+        public int Layer { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Param { get; set; }
+    }
+
+    public class BlockContainer
+    {
+        public List<Block> Blocks { get; set; } = new List<Block>();
+    }
+}
