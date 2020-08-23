3c56047cb540e9958dd939d2d019445b73925365
capasha <capashaa@gmail.com>
2020-08-23 16:42:04 +0200

    Updated blueprint to work like it should

--- EEditor/BluePrints.Designer.cs
+++ EEditor/BluePrints.Designer.cs
@@ -31,6 +31,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.SaveBPButton = new System.Windows.Forms.Button();
             this.LoadButton = new System.Windows.Forms.Button();
+            this.labelYN = new System.Windows.Forms.Label();
+            this.tBOwner = new System.Windows.Forms.TextBox();
+            this.buttonImport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,15 +41,15 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(380, 343);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // SaveBPButton
             // 
-            this.SaveBPButton.Location = new System.Drawing.Point(12, 361);
+            this.SaveBPButton.Location = new System.Drawing.Point(212, 418);
             this.SaveBPButton.Name = "SaveBPButton";
-            this.SaveBPButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveBPButton.Size = new System.Drawing.Size(53, 23);
             this.SaveBPButton.TabIndex = 1;
             this.SaveBPButton.Text = "Save";
             this.SaveBPButton.UseVisualStyleBackColor = true;
@@ -54,19 +57,48 @@
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(93, 361);
+            this.LoadButton.Location = new System.Drawing.Point(271, 418);
             this.LoadButton.Name = "LoadButton";
             this.LoadButton.Size = new System.Drawing.Size(75, 23);
             this.LoadButton.TabIndex = 2;
-            this.LoadButton.Text = "Load";
+            this.LoadButton.Text = "Load File..";
             this.LoadButton.UseVisualStyleBackColor = true;
             this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // labelYN
+            // 
+            this.labelYN.AutoSize = true;
+            this.labelYN.Location = new System.Drawing.Point(12, 423);
+            this.labelYN.Name = "labelYN";
+            this.labelYN.Size = new System.Drawing.Size(52, 13);
+            this.labelYN.TabIndex = 3;
+            this.labelYN.Text = "Made By:";
+            // 
+            // tBOwner
+            // 
+            this.tBOwner.Location = new System.Drawing.Point(81, 420);
+            this.tBOwner.Name = "tBOwner";
+            this.tBOwner.Size = new System.Drawing.Size(125, 20);
+            this.tBOwner.TabIndex = 4;
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(352, 418);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(60, 23);
+            this.buttonImport.TabIndex = 7;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
             // 
             // BluePrints
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(426, 450);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.tBOwner);
+            this.Controls.Add(this.labelYN);
             this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.SaveBPButton);
             this.Controls.Add(this.pictureBox1);
@@ -77,6 +109,7 @@
             this.Load += new System.EventHandler(this.BluePrints_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +118,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button SaveBPButton;
         private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label labelYN;
+        private System.Windows.Forms.TextBox tBOwner;
+        private System.Windows.Forms.Button buttonImport;
     }
 }
